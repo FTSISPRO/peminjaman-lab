cbcaa7c55db0cdf22afebc723c7bf1d6474c9e9e
arivanhi <arivanhikaru@gmail.com>
2023-06-05 09:02:43 +0700

    tambahan

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ function App() {
   const openNewTab = () => {
     window.open("https://s.id/peminjaman-lab", "_blank");
   };
+  const cards = [
+    {
+      title: "Alur Peminjaman Ruang",
+      description: "This is card 1",
+      image: "/images/alur_peminjaman_ruang.png",
+    },
+    {
+      title: "Alur Peminjaman Alat",
+      description: "This is card 2",
+      image: "/images/alur_peminjaman_alat.png",
+    },
+  ];
 
   return (
     <div className="App">
@@ -15,6 +27,15 @@ function App() {
         <button className="button" onClick={openNewTab}>
           Template Peminjaman
         </button>
+      </div>
+      <div className="card-container">
+        {cards.map((card, index) => (
+          <div className="card" key={index}>
+            <img src={card.image} alt={card.title} />
+            {/* <h3>{card.title}</h3> */}
+            {/* <p>{card.description}</p> */}
+          </div>
+        ))}
       </div>
     </div>
   );
